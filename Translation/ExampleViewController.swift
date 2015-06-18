ed24ad4aab97e3f44440803c87d2b4ecaf9a3c3f
榎本拓馬 <enomon0802@gmail.com>
2015-06-18 23:56:17 +0900

    first commit

---
+++ Translation/ExampleViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ExampleViewController.swift
+//  Translation
+//
+//  Created by 榎本拓馬 on 2015/06/18.
+//  Copyright (c) 2015年 榎本拓馬. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
