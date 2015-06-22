6bfdc09ec14cfcda7a36709fcf4712d60a9be59a
榎本拓馬 <enomon0802@gmail.com>
2015-06-22 23:56:38 +0900

    add dictionary

--- Translation/ResultViwController.swift
+++ Translation/ResultViwController.swift
@@ -12,6 +12,36 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
+    @IBOutlet weak var ResultTextlabel: UILabel!
+    
+    
+    
+    
+    @IBAction func result(){
+        
+        let myDefault = NSUserDefaults.standardUserDefaults()
+        
+        // 再度読み出し
+        if let readDict = myDefault.objectForKey("save") as? [String: String] {
+            let readData: String! = readDict["いらっしゃいませ"]
+            let readData2: String! = readDict["だめだ"]
+            let readData3: String! = readDict["ありがとう"]
+            ResultTextlabel.text = "\(readData)"
+            ResultTextlabel.text = "\(readData2)"
+            ResultTextlabel.text = "\(readData3)"
+        } else {
+            ResultTextlabel.text = "No such Data"
+        }
+        /*
+        Data of key1 = data
+        Data of key2 = data2
+        Data of key3 = nil
+        */
+        
+        
+        
+    }
+    
     
     
     
@@ -19,11 +49,13 @@ class ResultViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        var appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+//        var appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         //AppDelegateのインスタンスを取得
-        var message = appDelegate.message
+//        var message = appDelegate.message
         
     }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- Translation/TextViewController.swift
+++ Translation/TextViewController.swift
@@ -10,54 +10,57 @@ import Foundation
 
 import UIKit
 
-class TextViewController: UIViewController {
+class TextViewController: UIViewController , UITextFieldDelegate {
     
     @IBOutlet var text: UITextField!
     
 
     
-    @IBAction func array(){
+//    @IBAction func keyboard(sender: UITextField){
+//        text.returnKeyType = UIReturnKeyType.Search
+//        text.resignFirstResponder()
+//    }
     
+    
+    @IBAction func array(){
     // 何も無い状態をチェック
     let myDefault = NSUserDefaults.standardUserDefaults()
     if let readDict = myDefault.objectForKey("save") as? [String: String] {
-        let readData2 = readDict["key2"]!
+        let readData = readDict["いらっしゃいませ"]!
     } else {
     println("No such Data")
     }
     // No such data!
+        
+        
     // データを用意して保存
-    let saveData = ["key1": "data1", "key2": "data2", "key3": "data3"]
-    let saveDefault = NSUserDefaults.standardUserDefaults()
+    var saveData = ["いらっしゃいませ"/*key1*/: "おいでやす"/*data1*/, "だめだ": "あかん", "ありがとう": "おおきに"]
+    var saveDefault = NSUserDefaults.standardUserDefaults()
     saveDefault.setObject(saveData, forKey: "save")
     saveDefault.synchronize()
-    // 再度読み出し
-    if let readDict = myDefault.objectForKey("save") as? [String: String] {
-        let readData2: String! = readDict["key2"]
-        let readData3: String! = readDict["key3"]
-        let readData4: String! = readDict["key4"]
-        println("Data of key2 = (readData2)")
-        println("Data of key3 = (readData3)")
-        println("Data of key4 = (readData4)")
-    } else {
-    println("No such Data")
-    }
-    /*
-    Data of key2 = data2
-    Data of key3 = data3
-    Data of key4 = nil
-    */
+        
+
     // 消去
     saveDefault.removeObjectForKey("save")
     saveDefault.synchronize()
-    
+
     }
+    
+    
+    
     
     
     //翻訳するのが謎！
     @IBAction func change(){
     
     }
+    
+    
+    
+    
+    
+    
+    
     
     
     override func viewDidLoad() {
@@ -67,7 +70,7 @@ class TextViewController: UIViewController {
         //AppDelegateのインスタンスを取得
         appDelegate.message = "message"
         //appDelegateの変数を操作 
-        
+        self.text.delegate = self
         
     }
     
@@ -76,14 +79,11 @@ class TextViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    //@IBAction func text(sender:AnyObject){
-        
-        //var first = text.text
-        //Array.append(first)
-        //self.performSegueWithIdentifier("Hey!", sender: self)
-    //}
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
 }
 
 
